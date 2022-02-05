0e23d93605a228642ade3f7eb4b371884e77619d
Joanna Marie <joannamarie.saba@tup.edu.ph>
2022-02-05 23:47:45 +0800

    User CRUD

---
+++ src/pages/User/index.js
@@ -0,0 +1,121 @@
+import React, { useEffect, useState } from 'react';
+import { useIntl } from 'react-intl';
+import Page from 'material-ui-shell/lib/containers/Page';
+import { GridActionsCellItem } from '@mui/x-data-grid';
+import DataTable from '../../components/DataTable';
+import IconButton from '@mui/material/IconButton';
+import Box from '@mui/material/Box';
+import UserForm from './UserForm'
+import { useDispatch, useSelector } from "react-redux"
+import Stack from '@mui/material/Stack';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+import { useQuestions } from 'material-ui-shell/lib/providers/Dialogs/Question'
+import { useSnackbar } from 'notistack'
+import {
+  getAdminUsers,
+  deleteUser,
+  clearErrors,
+} from "../../actions/userActions"
+import { DELETE_USER_RESET } from "../../constants/userConstants"
+
+const User = ({history}) => {
+  const [page, setPage] = useState(0)
+  const [rowsPerPage, setRowsPerPage] = useState(10);
+  const [user, setUser] = useState({})
+  const intl = useIntl();
+  const dispatch = useDispatch()
+  const { openDialog, setProcessing } = useQuestions()
+  const { enqueueSnackbar } = useSnackbar()
+
+  const { loading, users, count, error } = useSelector((state) => state.users)
+  const { error: deleteError, isDeleted } = useSelector((state) => state.user)
+
+  useEffect(() => {
+    dispatch(getAdminUsers(page, rowsPerPage))
+    if (error === 'Unauthenticated.') {
+      console.log(history)
+      history.push('/signin')
+    }
+
+    if (deleteError) {
+      alert.error(deleteError)
+      dispatch(clearErrors())
+    }
+
+    if (isDeleted) {
+      dispatch({ type: DELETE_USER_RESET })
+      dispatch(getAdminUsers(page, rowsPerPage))
+      enqueueSnackbar('User successfully deleted.', {
+        variant: 'success',
+        anchorOrigin: {
+          vertical: 'top',
+          horizontal: 'center',
+        },
+      })
+    }
+  }, [dispatch, deleteError, isDeleted, page, rowsPerPage, error])
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 100, type: 'number'},
+    { field: 'name', headerName: 'Name', width: 150 },
+    { field: 'email', headerName: 'Email', width: 150 },
+    { field: 'year', headerName: 'Year', width: 150 },
+    { field: 'section', headerName: 'Section', width: 150 },
+    { field: 'school_id', headerName: 'School ID', width: 150 },
+    { field: 'role_id', headerName: 'Role ID', width: 150 },
+    { field: 'course_id', headerName: 'Course ID', width: 150 },
+    {
+      field: 'actions',
+      headerName: 'Actions',
+      type: 'actions',
+      disableExport: true,
+      getActions: (params) => [
+        <GridActionsCellItem icon={<EditIcon color="primary" />} onClick={() => setUser(params.row)} label="Edit" />,
+        <GridActionsCellItem icon={<DeleteIcon color="secondary" />} onClick={() => 
+          openDialog({
+            title: intl.formatMessage({
+              id: 'dialog_title',
+              defaultMessage: 'Dialog Item',
+            }),
+            message: intl.formatMessage({
+              id: 'dialog_message',
+              defaultMessage:
+                'Are you sure you want to delete this item?',
+            }),
+            action: intl.formatMessage({
+              id: 'dialog_action',
+              defaultMessage: 'YES, Delete',
+            }),
+            handleAction: (handleClose) => {
+              dispatch(deleteUser(params.id))
+              handleClose()
+            },
+          })
+        } label="Delete" />,
+      ]
+    },
+  ];
+
+  return (
+    <Page
+      pageTitle={intl.formatMessage({ id: 'user', defaultMessage: 'User' })}
+    >
+      <DataTable
+        rows={users}
+        columns={columns}
+        count={count}
+        loading={loading}
+        page={page}
+        setPage={setPage}
+        rowsPerPage={rowsPerPage}
+        setRowsPerPage={setRowsPerPage}
+      />
+
+      <Box>
+        <UserForm user={user} modalClosed={() => setUser({})} page={page} rowsPerPage={rowsPerPage} />
+      </Box>
+    </Page>
+  );
+};
+export default User;
