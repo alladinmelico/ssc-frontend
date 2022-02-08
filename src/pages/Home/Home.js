b67edcc59067c52fffe275d978fa222d604f9148
Alladin M. Melico <melico.alladin@gmail.com>
2022-02-08 14:59:59 +0800

    Update Home.js

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,6 @@ import { Typography } from '@mui/material'
 import Page from 'material-ui-shell/lib/containers/Page'
 import React from 'react'
 import { useIntl } from 'react-intl'
-import bootstrap from 'bootstrap'
 
 const HomePage = () => {
   const intl = useIntl()
