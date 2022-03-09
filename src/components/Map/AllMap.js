51b5f2491e53ebb6e67d456e6de9db84887b13e3
Joanna Marie <joannamarie.saba@tup.edu.ph>
2022-03-09 23:39:35 +0800

    firstfloor all in allmap

--- src/components/Map/AllMap.js
+++ src/components/Map/AllMap.js
@@ -1,5 +1,9 @@
 import React from "react"
 import Bsad from './Bsad'
+import Beng from './Beng'
+import Civil from './Civil'
+import Mech from './Mech'
+import Electrical from './Electrical'
 import Box from '@mui/material/Box';
 
 const AllMap = ({selected, setSelected, facilities}) => {
@@ -10,6 +14,38 @@ const AllMap = ({selected, setSelected, facilities}) => {
         <img id="gen-map-bg" src="/general-map.png" alt="tupt map" />
         <div id="gen-map-bsad">
           <Bsad
+            setSelected={setSelected}
+            selected={selected}
+            facilities={facilities}
+            hasShadow={true}
+          />
+        </div>
+        <div id="gen-map-beng">
+        <Beng
+            setSelected={setSelected}
+            selected={selected}
+            facilities={facilities}
+            hasShadow={true}
+          />
+        </div>
+        <div id="gen-map-civil">
+        <Civil
+            setSelected={setSelected}
+            selected={selected}
+            facilities={facilities}
+            hasShadow={true}
+          />
+        </div>
+        <div id="gen-map-mech">
+        <Mech
+            setSelected={setSelected}
+            selected={selected}
+            facilities={facilities}
+            hasShadow={true}
+          />
+        </div>
+        <div id="gen-map-electrical">
+          <Electrical
             setSelected={setSelected}
             selected={selected}
             facilities={facilities}
