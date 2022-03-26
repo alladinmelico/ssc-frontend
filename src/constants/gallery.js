da04807aa38f42eba30d38292f08522abd59c180
patr-espenida <patriciamae.espenida@gmail.com>
2022-03-26 08:17:19 +0800

    added new pictures for the map gallery

--- src/constants/gallery.js
+++ src/constants/gallery.js
@@ -1,14 +1,84 @@
 const gallery = [
   {
+    name: 'Automotive Engineering Technology',
+    image: '/gallery/beng1.jpg',
+    department: 1,
+  },
+  {
+    name: 'Inside the Automotive Engineering Technology',
+    image: '/gallery/beng2.jpg',
+    department: 1,
+  },
+  {
+    name: 'Heating, Ventilation and Airconditioning/Refrigeration Technology',
+    image: '/gallery/beng3.jpg',
+    department: 1,
+  },
+
+  {
+    name: 'Basic Arts and Sciences (BSAD)',
+    image: '/gallery/basd1.jpg',
+    department: 2,
+  },
+  {
+    image: '/gallery/basd2.jpg',
+    department: 2,
+  },
+
+  {
+    name: 'Civil Engineering Technology',
+    image: '/gallery/civil1.jpg',
+    department: 3,
+  },
+  {
+    image: '/gallery/civil2.jpg',
+    department: 3,
+  },
+  {
+    image: '/gallery/civil3.jpg',
+    department: 3,
+  },
+
+  {
     name: 'IECEP',
     image: '/gallery/ee1.jpg',
     department: 4,
   },
   {
-    name: 'Non-destructive Testing Engineering',
+    name: 'Italian Building',
+    image: '/gallery/ee2.jpg',
+    department: 4,
+  },
+  {
+    name: 'Electro-Mechanical Engineering',
+    image: '/gallery/ee3.jpg',
+    department: 4,
+  },
+
+  {
+    name: 'Mechanical and Allied Department',
+    image: '/gallery/mech1.jpg',
+    department: 5,
+  },
+  {
+    name: 'Tool Engineering Area',
+    image: '/gallery/mech2.jpg',
+    department: 5,
+  },
+  {
+    name: 'Chemical Technology',
+    image: '/gallery/chemtech1.jpg',
+    department: 5,
+  },
+  {
+    image: '/gallery/chemtech2.jpg',
+    floor: 3,
+  },
+  {
+    name: 'Non-Destructive Testing Technology',
     image: '/gallery/ndt1.jpg',
-    department: 1,
-  }
+    department: 5,
+  },
 ]
 
 export default gallery
