31d4c66745a8751ed50648f57acd3c6be1b232c9
alladin.melico <alladin.melico@appetiser.com.au>
2022-09-09 13:29:42 +0800

    import temp

--- src/pages/Temperature/index.js
+++ src/pages/Temperature/index.js
@@ -4,6 +4,11 @@ import Page from 'material-ui-shell/lib/containers/Page';
 import DataTable from '../../components/DataTable';
 import { useSnackbar } from 'notistack'
 import API from 'config/api';
+import Button from '@mui/material/Button';
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined';
+import FormModal from '../../components/Modal/FormModal'
+import TextField from '@mui/material/TextField';
+import MainAppBar from 'components/MainAppBar'
 
 const Temperature = ({history}) => {
   const [page, setPage] = useState(0)
@@ -13,6 +18,10 @@ const Temperature = ({history}) => {
   const [count, setCount] = useState(0);
   const intl = useIntl();
   const { enqueueSnackbar } = useSnackbar()
+  const [openModal, setOpenModal] = useState(false)
+  const [error, setError] = useState('')
+  const [success, setSuccess] = useState(false)
+  const [imports, setImports] = useState('')
 
   const getData = async () => {
     setLoading(true)
@@ -41,9 +50,80 @@ const Temperature = ({history}) => {
     { field: 'temperature', headerName: 'Temperature', width: 150 },
   ];
 
+  async function importTemperatures () {
+    setLoading(true)
+    await API.post(`import-temperature`, {temperatures: imports})
+    .then(res => {
+      if (res.status === 200) {
+        setSuccess(true)
+        getData()
+        enqueueSnackbar('Temperatures successfully imported.', {
+          variant: 'success',
+          anchorOrigin: {
+            vertical: 'top',
+            horizontal: 'center',
+          },
+        })
+      }
+    })
+    .catch(err => {
+      if (err.response && err.response.status === 404) {
+        setError('Invalid invite code.')
+      }
+    })
+    .finally(() => {
+      setLoading(false)
+    })
+  }
+
+  const onSubmit = event => {
+    event.preventDefault();
+    importTemperatures()
+  };
+
   return (
     <Page
-      pageTitle={intl.formatMessage({ id: 'temperature', defaultMessage: 'Temperature' })}
+      appBarContent={
+        <MainAppBar
+          title={intl.formatMessage({ id: 'temperature', defaultMessage: 'Temperature' })}
+          noBack
+          tools={
+            <div>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={() => setOpenModal(true)}
+                aria-label="add"
+                style={{ backgroundColor: 'transparent', marginRight: '1rem' }}
+                startIcon={<AddBoxOutlinedIcon />}
+              >
+                Import Temperatures
+              </Button>
+              <FormModal
+                title={"Import Temperatures"}
+                btnType="secondary"
+                onSubmit={onSubmit}
+                success={success}
+                loading={loading}
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+              >
+                <TextField 
+                  label="Temperatures"
+                  variant="outlined"
+                  value={imports}
+                  required
+                  onChange={(event) => setImports(event.target.value)}
+                  margin="normal"
+                  error={!!error}
+                  helperText="Use comma(,) to separate values"
+                  fullWidth
+                />
+              </FormModal>
+            </div>
+          }
+        />
+      }
     >
       <DataTable
         rows={temperatures.map(temp => ({...temp, user_name:  temp.user?.name}))}
